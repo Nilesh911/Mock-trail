36bdea10b703c8a51382e684c87e881a0ba30a6a
Nilesh119 <117853068+Nilesh119@users.noreply.github.com>
2023-07-10 16:52:35 +0530

    try

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,10 +12,14 @@ function Home() {
       <Link to= '/new-ticket' className='btn btn-reverse btn-block'>
       <FaQuestionCircle/> Create New Ticket
       </Link>
+    
 
       <Link to= '/ticket' className='btn  btn-block'>
       <FaTicketAlt/> view My Tickets
       </Link>
+      <div className="main">
+        <h1>this is the Change</h1>
+      </div>
     </>
   )
 }
